fa18fc722f20524013f055e1886cae6833ddea70
Muhannad Alrusayni <Muhannad.Alrusayni@gmail.com>
2021-09-23 22:28:04 +0300

    prototype design for ListNewsApi using tailwindcss

--- src/components/list-news-api.tsx
+++ src/components/list-news-api.tsx
@@ -17,13 +17,16 @@ export class ListNewsApi extends React.Component {
 
 function into_rows(articals: Artical[]): JSX.Element[] {
     return articals.map((artical: Artical, index: number) => (
-        <div key={index}>
-            <h1>{artical.title}</h1>
-            <p>Author: {artical.author}</p>
-            <p>Published at: {artical.publishedAt}</p>
-            <p>URL: {artical.url}</p>
-            <p>imgURL: {artical.urlToImg}</p>
-            <p>description: {artical.description}</p>
+        <div key={index} className="p-7 bg-white flex shadow items-center space-x-4 space-y-2 bg-white flex shadow items-center space-x-4 space-y-2">
+            <div className="w-96 h-52 items-center flex">
+                <img className="object-contain h-full w-full" src={artical.urlToImg} alt="News Image" />
+            </div>
+            <div className="flex flex-col items-start space-y-1">
+                <div className="text-xl font-md text-black">{artical.title}</div>
+                <div className="text-sm font-large text-gray-500">{artical.description}</div>
+                <div className="text-sm font-large text-gray-500">Author: {artical.author}, published at: {artical.publishedAt}</div>
+                <div className="text-sm font-large text-gray-500">Source: {artical.url}</div>
+            </div>
         </div>
     ))
 }
